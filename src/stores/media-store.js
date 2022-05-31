92bc5ff9bb6faa5ce20b0c4e245e105c30479270
Hans Dorrestijn <hansie211@hotmail.com>
2022-05-31 11:35:38 +0200

    Validate new items

--- src/stores/media-store.js
+++ src/stores/media-store.js
@@ -16,6 +16,21 @@ export const useMediaStore = defineStore("MediaStore", {
             storeItem.url = item.url;
         },
         addItem(item) {
+            if (!item) {
+                console.error(`Error: item is null`);
+                return;
+            }
+
+            if (!item.id) {
+                console.error(`Error: item.id is null (${JSON.stringify(item)})`);
+                return;
+            }
+
+            if (!item.url) {
+                console.error(`Error: item.id url null (${JSON.stringify(item)})`);
+                return;
+            }
+
             const idx = this.mediaItems.findIndex((m) => m.id === item.id);
             if (idx >= 0) {
                 this.updateItem(this.mediaItems[idx], item);
